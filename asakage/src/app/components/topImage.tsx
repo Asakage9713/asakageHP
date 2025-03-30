a63b2f2fc4d7c871b07ce883fd9a0966de86fa01
asakage9713 <horoyoi.hicee@gmail.com>
2025-03-31 01:22:48 +0900

    feature:トップの画像をレスポンシブ対応

--- asakage/src/app/components/topImage.tsx
+++ asakage/src/app/components/topImage.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function TopImage () {
   return (
-    <div className="relative w-full h-screen">
+    <div className="relative w-full min-h-[400px] sm:min-h-[500px] md:min-h-[600px] lg:min-h-screen xl:min-h-screen">
       <Image 
+        className="sm:object-contain md:object-cover lg:object-cover"
         src="/topImage/topImage.png" 
         alt="Top Landing Image" 
         layout="fill" 
@@ -11,6 +12,7 @@ export default function TopImage () {
         objectPosition="left top"
         priority
       />
+      {/*名前等のコメントを入れたいならここに記載 */}
       {/* <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-3xl">
         Welcome to My Next.js App
       </h1> */}
